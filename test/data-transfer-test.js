6b6705e598e9fb8189974b95e692041a231ca3e6
Charles Kaminer <ckamine@g.clemson.edu>
2016-08-24 11:52:47 -0600

    complete single row right direction implementation tested all cases for single row compacting and doubling full functional for moving right all tests passing

--- test/data-transfer-test.js
+++ test/data-transfer-test.js
@@ -5,7 +5,7 @@ const NodeList = require('../lib/node-list');
 const DataTransfer = require('../lib/data-transfer');
 
 describe('DataTransfer', function(){
-  context('can transfer data of one row', function(){
+  context('can transfer data of one row going right', function(){
     it('should double the number and move to the right edge', function(){
       var staticNode1 = new StaticNode({ data: null, leftNode: "edge" });
       var staticNode2 = new StaticNode({ data: 4, leftNode: staticNode1 });
@@ -40,7 +40,7 @@ describe('DataTransfer', function(){
       assert.equal(staticNode1.data, null);
     });
 
-    it('should leave all numbers alone and move them to the right', function(){
+    it('no number value change and move them to the right', function(){
       var staticNode1 = new StaticNode({ data: 2, leftNode: "edge" });
       var staticNode2 = new StaticNode({ data: 4, leftNode: staticNode1 });
       var staticNode3 = new StaticNode({ data: 8, leftNode: staticNode2 });
@@ -136,19 +136,27 @@ describe('DataTransfer', function(){
       var topRow = new NodeList({head: staticNode4, direction: "right"});
 
       DataTransfer(topRow);
-      assert.equal(staticNode4.data, 2);
+      assert.equal(staticNode4.data, 4);
+      assert.equal(staticNode3.data, null);
+      assert.equal(staticNode2.data, null);
+      assert.equal(staticNode1.data, null);
+    });
+
+    it('should move far left to far right and double', function(){
+      var staticNode1 = new StaticNode({ data: 2, leftNode: "edge" });
+      var staticNode2 = new StaticNode({ data: null, leftNode: staticNode1 });
+      var staticNode3 = new StaticNode({ data: null, leftNode: staticNode2 });
+      var staticNode4 = new StaticNode({ data: 2, leftNode: staticNode3, rightNode: "edge" });
+      staticNode1.rightNode = staticNode2;
+      staticNode2.rightNode = staticNode3;
+      staticNode3.rightNode = staticNode4;
+      var topRow = new NodeList({head: staticNode4, direction: "right"});
+
+      DataTransfer(topRow);
+      assert.equal(staticNode4.data, 4);
       assert.equal(staticNode3.data, null);
       assert.equal(staticNode2.data, null);
       assert.equal(staticNode1.data, null);
     });
   });
 });
-
-  // Given a top row of 4 slots
-  // with the second slot having a 4
-  // and the third slot having a 4
-  // when a right motion is registered
-  // then I should have a top row of 4 slots
-  // where slot 1, 2 and 3 are empty
-  // and 4 is full
-  // and 4 has a value of 8
