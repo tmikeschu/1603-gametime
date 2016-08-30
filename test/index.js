7f675669ec400fa8b4ffca3d6ca0f6bda43b446b
Ilana Corson <ilana.corson@gmail.com>
2016-08-30 13:20:50 -0600

    Game brain (#7)
    * Game brain - working on game brain to generate tiles
    
    - add more variables exported to make for easy access
    - change the tiles to be objects of numbers pointing to coordinates instead of numbers as ids
    - change the functionality of the generate tile to generate a tile with a current Node value
    
    * working on the brain and the board and clearing the tiles on new game
    
    * game board fix 
    fixed the game board functionality so that it will populate two random tiles on 'start game'
    
    * Done with MVP 
    - the game is fully functional 
    - working towards images 
    - working towards animation

--- test/index.js
+++ test/index.js
@@ -2,29 +2,3 @@ require('./tile-test.js');
 require('./static-node-test.js');
 require('./node-list-test.js');
 require('./data-transfer-test.js');
-
-// test scenario
-
-// var board = new Board
-// board is all set up
-// assertions around what the board has
-// board.registerChange()
-// assertions that things you asserted earlier have changed
-
-// Given a top row of 4 slots
-// with the second slot having a 4
-// and the third slot having a 4
-// when a right motion is registered
-// then I should have a top row of 4 slots
-// where slot 1, 2 and 3 are empty
-// and 4 is full
-// and 4 has a value of 8
-
-// Given a full board
-// same scenerio
-// same result
-
-// Similar scenario with middle row
-
-// Same scenario as first
-// where numbers don't match
